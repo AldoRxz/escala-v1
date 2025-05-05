8dfd7e38ec5afb4a42c2c697894266877d9abf20
Aldo <aldo_roman13@hotmail.com>
2025-05-05 00:37:36 -0600

    chore: integrate new marketing components into the page

--- app/marketing-digital/page.tsx
+++ app/marketing-digital/page.tsx
@@ -1,15 +1,30 @@
 import Header from "@/components/header"
 import Footer from "@/components/footer"
 
+import DigitalMarketing from "@/components/digital-marketing"
+import MarketingHero from "@/components/marketing/MarketingHero"
+import ResultadosMarketing from "@/components/marketing/ResultadosMarketing"
+import MarketingContactForm from "@/components/marketing/MarketingContactForm"
+import MarketingOverview from "@/components/marketing/MarketingOverview"
+import SocialMediaManagement from "@/components/marketing/SocialMediaManagement"
+
+
+import CallToAction from "@/components/CallToAction"
+
 export default function MarketingDigitalPage() {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <main className="flex-1 p-10">
-        <h1 className="text-3xl font-bold mb-4">Marketing Digital</h1>
-        <p className="text-lg text-gray-700">
-          Estrategias creativas para destacar tu marca en redes sociales, buscadores y más.
-        </p>
+      <main className="flex-1">
+        <MarketingHero />
+        <MarketingOverview />
+        {/* <SocialMediaManagement /> */}
+        <DigitalMarketing />
+        <ResultadosMarketing />
+        <MarketingContactForm />
+        <SocialMediaManagement />
+
+        <CallToAction />
       </main>
       <Footer />
     </div>
